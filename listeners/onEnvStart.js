78e46278c5f4d23e1fd9df7fe05b9e6fce0fd8df
Emric Pichonnier <emric.pichonnier@lenra.io>
2022-05-09 11:07:41 +0200

    remove catch

--- listeners/onEnvStart.js
+++ listeners/onEnvStart.js
@@ -6,5 +6,5 @@ const counterService = require("../services/counterService");
 module.exports = (props, event, api) => {
     return counterService.createDatastore(api).then(function (response) {
         response.data
-    }).catch((e => { }));
+    });
 }
