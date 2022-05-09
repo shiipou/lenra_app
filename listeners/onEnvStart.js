3fcd9838a23de2d942f0a6ee791507c06b4f9e08
Emric Pichonnier <emric.pichonnier@lenra.io>
2022-05-09 11:19:12 +0200

    add catch

--- listeners/onEnvStart.js
+++ listeners/onEnvStart.js
@@ -4,9 +4,7 @@ const counterService = require("../services/counterService");
 
 
 module.exports = (props, event, api) => {
-    console.log("OnEnvStart")
     return counterService.createDatastore(api).then(function (response) {
-        console.log("response" + response)
         response.data
-    });
+    }).catch((e => { }));
 }
